527bfe4d0b6b44e31d4893d8ad2d3bdb26e05c54
GibzClt <gibinninan5@gmail.com>
2021-11-06 22:35:46 +0530

    ui of categories and sorting done

--- src/components/productsPage/ProductsPage.js
+++ src/components/productsPage/ProductsPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import NavigationBar from "../../common/navigationBar/NavigationBar";
 import Card from '@mui/material/Card';
 import CardMedia from '@mui/material/CardMedia';
@@ -11,13 +11,43 @@ import Grid from '@mui/material/Grid';
 import "./ProductsPage.css";
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 import products from '../../assets/products.json';
+import {Select, MenuItem} from "@mui/material"
 
 function ProductsPage({isAdmin}){
-  console.log(isAdmin);
+  const categories = ['All', 'Apparel', 'Electronics', 'Footwear', 'Personal Care'];
+  const [toggleValue, setToggleValue] = useState('All');
+  const [selectValue, setSelectValue] = useState('default');
+  
+  const handleChange=(event, type)=>{
+    const {value} = event.target;
+    type === "toggle" ? setToggleValue(value) : setSelectValue(value);
+  }
+
   return(
     <>
       <NavigationBar />
+        <div id="toggle-btn-container">
+          <ToggleButtonGroup exclusive={true} value={toggleValue} onChange={e=>handleChange(e,"toggle")}>
+            {categories.map(item=>(
+              <ToggleButton value={item}>{item}</ToggleButton>
+            ))}
+          </ToggleButtonGroup>
+        </div>
+        <div id="select-container">
+          <Select
+            sx={{width : "20%"}}
+            onChange={e=>handleChange(e,"select")}
+            value={selectValue}
+          >
+            <MenuItem value="default">Default</MenuItem>
+            <MenuItem value="price-htol">Price : High to Low</MenuItem>
+            <MenuItem value="price-ltoh">Price : Low to High</MenuItem>
+            <MenuItem value="newest">Price : Newest</MenuItem>
+          </Select>
+        </div>
         <Grid id="grid-container" container columns={8} columnSpacing={5} rowSpacing={5}>
           {products.map(item=>(
             <Grid item lg={2} md={2} xs={2} sx={{display : "flex"}} >
