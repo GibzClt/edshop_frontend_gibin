28d18161af926f6dfec3bdd481b48dd4bdd0d382
GibzClt <gibinninan5@gmail.com>
2021-11-08 22:35:24 +0530

    UI of product details page ready

--- src/index.js
+++ src/index.js
@@ -5,9 +5,12 @@ import NavigationBar from "./common/navigationBar/NavigationBar";
 import SignIn from "./components/signIn/SignIn";
 import SignUp from "./components/signUp/SignUp";
 import ProductPage from "./components/productsPage/ProductsPage";
+import ProductDetails from "./components/productDetails/ProductDetails";
+import Products from "./assets/products.json";
 
 
 ReactDOM.render(
-  <ProductPage isAdmin/>,
+  // <ProductPage isAdmin/>
+  <ProductDetails product={Products}/>,
   document.getElementById('root')
 );
