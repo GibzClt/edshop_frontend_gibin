a8493fe841b596533be6afcf4bd16864260e14ea
GibzClt <gibinninan5@gmail.com>
2021-11-09 18:06:00 +0530

    address page done

--- src/index.js
+++ src/index.js
@@ -7,10 +7,11 @@ import SignUp from "./components/signUp/SignUp";
 import ProductPage from "./components/productsPage/ProductsPage";
 import ProductDetails from "./components/productDetails/ProductDetails";
 import Products from "./assets/products.json";
-
+import OrderPage from "./components/orderPage/OrderPage";
 
 ReactDOM.render(
   // <ProductPage isAdmin/>
-  <ProductDetails product={Products}/>,
+  // <ProductDetails product={Products}/>
+  <OrderPage product={Products} quantity={2} />,
   document.getElementById('root')
 );
